ad290bcedaf45aa6715a75e8f6c7731c22bd6e1f
Benjamin Torres <benjamin@unstructured.io>
2022-11-28 11:42:58 -0600

    test: add tests for multipage PDFs

--- test_document_layout/api/test_layout.py
+++ test_document_layout/api/test_layout.py
@@ -9,29 +9,10 @@ DIRECTORY = Path(__file__).absolute().parent
 
 SAMPLE_DOCS_DIRECTORY = os.path.join(DIRECTORY, "..", "..", "sample-docs")
 
-
 LAYOUT_ROUTE = get_pipeline_path("layout", pipeline_family="document-layout", semver="0.0.1")
 
-# def test_memo_api():
-#     filename = os.path.join(SAMPLE_DOCS_DIRECTORY, "sample-memo.pdf")
-#     app.state.limiter.reset()
-#     client = TestClient(app)
-#     response = client.post(
-#         MEMO_ROUTE,
-#         files={
-#             "files": (
-#                 filename,
-#                 open(filename, "rb"),
-#             )
-#         },
-#     )
-
-#     assert response.status_code == 200
-#     assert response.json() == EXPECTED_RESPONSE
-
-
 def test_layout_pdf():
-    filename = "/Users/benjamin/Documents/pipeline-document-layout/sample-docs/example.pdf"
+    filename = "../pipeline-document-layout/sample-docs/example.pdf"
     app.state.limiter.reset()
     client = TestClient(app)
     response = client.post(
@@ -39,6 +20,34 @@ def test_layout_pdf():
         headers={"Accept": "multipart/mixed"},
         files=[
             ("files", (filename, open(filename, "rb"), "application/pdf")),
+        ],
+    )
+
+    assert response.status_code == 200
+
+def test_layout_multipage_pdf():
+    filename = "../pipeline-document-layout/sample-docs/example-multipage.pdf"
+    app.state.limiter.reset()
+    client = TestClient(app)
+    response = client.post(
+        LAYOUT_ROUTE,
+        headers={"Accept": "multipart/mixed"},
+        files=[
+            ("files", (filename, open(filename, "rb"), "application/pdf")),
+        ],
+    )
+
+    assert response.status_code == 200
+
+def test_layout_image():
+    filename = "../pipeline-document-layout/sample-docs/example.png"
+    app.state.limiter.reset()
+    client = TestClient(app)
+    response = client.post(
+        LAYOUT_ROUTE,
+        headers={"Accept": "multipart/mixed"},
+        files=[
+            ("files", (filename, open(filename, "rb"), "image/png")),
         ],
     )
 
